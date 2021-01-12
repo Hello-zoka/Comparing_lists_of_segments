741657809bec0848cdbdbca31e310f48edaec0c6
Hello-zoka <yura.zajcev.2003@mail.ru>
2021-01-12 17:11:41 +0300

    Added working with user functions. Reading and printing list of segments in Console. TODO: make file input/output, add functions for merging segmets

---
+++ WorkingWithUser.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include "hedaer.h"
+
+using namespace std;
+
+
+vector<segment> ReadInput() {
+    int num_of_segments;
+    vector<segment> list_of_nodes;
+    cin >> num_of_segments;
+    for (int i = 0; i < num_of_segments; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        list_of_nodes.push_back({{x1, y1},
+                                 {x2, y2}});
+    }
+    return list_of_nodes;
+}
+
+void PrintOutput(vector<segment> &list_of_segments) {
+    cout << "Number of segemts: " << list_of_segments.size() << '\n';
+    for (segment cur_segment : list_of_segments) {
+        cout << "x1: " << cur_segment.first_point.x << " y1: " << cur_segment.first_point.y << " x2: "
+             << cur_segment.second_point.x << " y2: " << cur_segment.second_point.y << '\n';
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include "hedaer.h"
 
 using namespace  std;
 
 int main() {
-    cout << "hey everybody";
-    cout << "and you too";
+    vector<segment> list_of_segments = ReadInput();
+    //Merge(lsit_of_nodes);
+    PrintOutput(list_of_segments);
     return 0;
 }
