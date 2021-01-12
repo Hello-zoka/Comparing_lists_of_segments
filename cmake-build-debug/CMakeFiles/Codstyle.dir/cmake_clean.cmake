3e52f948586215f2c28a9ce654bd07870d4f6457
Hello-zoka <yura.zajcev.2003@mail.ru>
2021-01-12 15:08:01 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Codstyle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Codstyle.dir/main.cpp.o"
+  "Codstyle"
+  "Codstyle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Codstyle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
